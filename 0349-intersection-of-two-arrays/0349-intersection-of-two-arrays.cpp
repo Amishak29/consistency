6ecfdcdee0de0d53bf8103f224e4f61ac949ce28
Amisha Arunjay Kumar <78794164+Amishak29@users.noreply.github.com>
2023-05-28 15:33:03 +0530

    Time: 4 ms (79.74%), Space: 10.3 MB (41.06%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -5,11 +5,11 @@ public:
 
     vector<int> intersection;
     for (int num : arr2) {
-        if (set1.count(num) > 0) {
-            intersection.push_back(num);
-            set1.erase(num); // Remove the element to handle duplicates
-        }
+    if (set1.find(num) != set1.end()) {
+        intersection.push_back(num);
+        set1.erase(num);
     }
+}
 
     return intersection;
     }
