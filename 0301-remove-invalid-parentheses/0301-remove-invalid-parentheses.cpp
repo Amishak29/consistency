32d45ca83ccb5cdc71b9cd2d5625405f98e6e820
Amisha Arunjay Kumar <78794164+Amishak29@users.noreply.github.com>
2023-06-15 22:25:08 +0530

    Time: 220 ms (46.02%), Space: 49.9 MB (33.27%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
@@ -1,58 +1,61 @@
 class Solution {
 public:
+    vector<string> res; // Stores the valid parentheses expressions
+    unordered_map<string, int> mp; // Used for memoization to avoid recomputation
     
-    vector<string> res;
-    unordered_map<string,int> mp;
-    
-    int getMinInValid(string s)   //calculate minimum length after removal makes valid parenthesis
-    {
+    // Helper function to calculate the minimum length of parentheses to remove
+    int getMinInValid(string s) {
         stack<char> stck;
         int i = 0;
-        while(i < s.size())
-        {
-            if(s[i] == '(')
+        
+        while (i < s.size()) {
+            if (s[i] == '(')
                 stck.push('(');
-            else if(s[i] == ')')
-            {
-                if(stck.size() > 0 && stck.top() == '(')
+            else if (s[i] == ')') {
+                if (stck.size() > 0 && stck.top() == '(')
                     stck.pop();
                 else
                     stck.push(')');
             }
             i++;
         }
+        
         return stck.size();
     }
     
-    void solve(string s,int minInv){
-
-        if(mp[s] != 0) 
-            return;
+    // Recursive function to find all valid parentheses expressions
+    void solve(string s, int minInv) {
+        if (mp[s] != 0)
+            return; // Skip if the current expression is already computed
         else
-            mp[s]++; //mp[s] = 1
-        //base case
-        if(minInv < 0){
+            mp[s]++; // Mark the expression as computed
+        
+        // Base case: if minimum invalid count is less than 0, the expression is invalid
+        if (minInv < 0) {
             return;
         }
-        if(minInv == 0)
-        {
-            if(!getMinInValid(s))
+        
+        // Base case: if minimum invalid count is 0 and the expression is valid,
+        // add it to the result vector
+        if (minInv == 0) {
+            if (!getMinInValid(s))
                 res.push_back(s);
             return;
         }
         
-        for(int i=0; i<s.size(); i++)
-        {
-            string left = s.substr(0,i);
-            string right = s.substr(i+1);
-            solve(left+right, minInv-1);
+        // Recursive case: remove parentheses one by one and recursively call the function
+        for (int i = 0; i < s.size(); i++) {
+            string left = s.substr(0, i); // Expression before the current parentheses
+            string right = s.substr(i + 1); // Expression after the current parentheses
+            solve(left + right, minInv - 1);
         }
+        
         return;
     }
     
-    vector<string> removeInvalidParentheses(string s) 
-    {
-        solve(s, getMinInValid(s));
-        return res;
+    // Main function to remove invalid parentheses
+    vector<string> removeInvalidParentheses(string s) {
+        solve(s, getMinInValid(s)); // Start the recursive process
+        return res; // Return the valid parentheses expressions
     }
 };
