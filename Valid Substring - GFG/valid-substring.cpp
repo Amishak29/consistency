d69e7d8efb029c3b3234862790b675c638fe238a
Amisha Arunjay Kumar <78794164+Amishak29@users.noreply.github.com>
2023-06-22 00:04:26 +0530

    Added solution - LeetHub

---
+++ Valid Substring - GFG/valid-substring.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for c++
+class Solution {
+  public:
+    int findMaxLen(string s) {
+    stack<int> st;
+    st.push(-1); // Initialize stack with -1
+    
+    int longestLength = 0;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '(') {
+            st.push(i); // Push the index of opening parenthesis
+        } else if (s[i] == ')') {
+            if (!st.empty()) {
+                st.pop(); // Pop the index of matching opening parenthesis
+                if (!st.empty()) {
+                    int currentLength = i - st.top();
+                    longestLength = std::max(longestLength, currentLength);
+                } else {
+                    st.push(i); // Push the index of unmatched closing parenthesis
+                }
+            }
+        }
+    }
+    
+    return longestLength;
+}
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S; 
+        Solution ob;
+        cout << ob.findMaxLen(S) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
