38ef8d7ac3edee7141c5908940251e7f6539bda6
Amisha Arunjay Kumar <78794164+Amishak29@users.noreply.github.com>
2023-06-25 17:31:21 +0530

    Added solution - LeetHub

--- Stickler Thief - GFG/stickler-thief.cpp
+++ Stickler Thief - GFG/stickler-thief.cpp
@@ -8,29 +8,22 @@ class Solution
 {
     public:
     //Function to find the maximum money the thief can get.
-    int FindMaxSum(int arr[], int n) {
-    // Condition when there are no houses
-    if (n == 0)
-        return 0;
-    // Condition when there is only one house
-    if (n == 1)
-        return arr[0];
-
-    // Taking an extra array with size n
-    int dp[n];
-
-    // Initializing the first and second element for two houses
-    dp[0] = arr[0];
-    dp[1] = max(arr[0], arr[1]);
-
-    // Iterating from the 2nd house to the last house
-    for (int i = 2; i < n; i++) {
-        dp[i] = max(dp[i - 2] + arr[i], dp[i - 1]);
+    int FindMaxSum(int nums[], int n)
+    {
+        vector<int> dp(n);
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        dp[0]=nums[0];
+        dp[1]=max(nums[0], nums[1]);
+        
+        for(int i=2; i<n; i++)
+        {
+            dp[i]=max(dp[i-2]+nums[i], dp[i-1]);
+        }
+        
+        return dp[n-1];
+    
     }
-
-    return dp[n - 1];
-}
-
 };
 
 //{ Driver Code Starts.
