58410549949334bb2244f41a88b4b8a45c4d6896
Amisha Arunjay Kumar <78794164+Amishak29@users.noreply.github.com>
2023-06-26 23:06:26 +0530

    Added solution - LeetHub

--- Kadane's Algorithm - GFG/kadanes-algorithm.cpp
+++ Kadane's Algorithm - GFG/kadanes-algorithm.cpp
@@ -6,38 +6,38 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    // arr: input array
-    // n: size of array
-    //Function to find the sum of contiguous subarray with maximum sum.
-    long long maxSubarraySum(int arr[], int n){
-        
-    long currsum=0;
-      long maxsum=LONG_MIN;
-        for(int i=0;i<n;i++){
-            currsum+=arr[i];
-            if(currsum>maxsum){
-                maxsum=currsum;
-            }
-            if(currsum<0){
-                currsum=0;
-            }
+// arr: input array
+// n: size of array
+//Function to find the sum of contiguous subarray with maximum sum.
+long long maxSubarraySum(int arr[], int n){
+long currsum=0;
+  long maxsum=LONG_MIN;
+    for(int i=0;i<n;i++){
+        currsum+=arr[i];
+        if(currsum>maxsum){
+            maxsum=currsum;
         }
-        return maxsum;
+        if(currsum<0){
+            currsum=0;
+        }
     }
-        // long long maxsum = arr[0];
-        // long long cursum = arr[0];
-        // for (int i = 1; i < n; i++) {
-        //     cursum += arr[i];
-        //     if (arr[i] > cursum) {
-        //         cursum = arr[i];
-        //     }
-        //     if (cursum > maxsum) {
-        //         maxsum = cursum;
-        //     }
-        // }
-        // return maxsum;
-        
-        //     }
+    return maxsum;
+}
+    // long long maxsum = arr[0];
+    // long long cursum = arr[0];
+    // for (int i = 1; i < n; i++) {
+    //     cursum += arr[i];
+    //     if (arr[i] > cursum) {
+    //         cursum = arr[i];
+    //     }
+    //     if (cursum > maxsum) {
+    //         maxsum = cursum;
+    //     }
+    // }
+    // return maxsum;
+
+    //     }
+
 };
 
 //{ Driver Code Starts.
