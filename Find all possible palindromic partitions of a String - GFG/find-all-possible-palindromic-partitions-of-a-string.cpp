ac3d74b75ceba7c6f646138df6da71057afc7f69
Amisha Arunjay Kumar <78794164+Amishak29@users.noreply.github.com>
2023-08-01 15:37:34 +0530

    Added solution - LeetHub

--- Find all possible palindromic partitions of a String - GFG/find-all-possible-palindromic-partitions-of-a-string.cpp
+++ Find all possible palindromic partitions of a String - GFG/find-all-possible-palindromic-partitions-of-a-string.cpp
@@ -7,34 +7,33 @@ using namespace std;
 
 class Solution {
   public:
-    vector<vector<string>> ans; 
-    
-    bool ispalindrome(string s,int start,int end){
-        while(start<=end){
-            if(s[start]!=s[end])return false;
-            start++;
-            end--;
-        }
-        return true;
+    vector<vector<string>> ans;
+bool ispalindrome(string s,int start,int end){
+    while(start<=end){
+        if(s[start]!=s[end])return false;
+        start++;
+        end--;
     }
-    void partioning(string s,int index,int n,vector<string> &path){
-        if(index==n){
-            ans.push_back(path);
-            return;
-        }
-        for(int i=index;i<n;i++){
-            if(ispalindrome(s,index,i)){
-                path.push_back(s.substr(index,i-index+1));
-                partioning(s,i+1,n,path);
-                path.pop_back();
-            }
+    return true;
+}
+void partioning(string s,int index,int n,vector<string> &path){
+    if(index==n){
+        ans.push_back(path);
+        return;
+    }
+    for(int i=index;i<n;i++){
+        if(ispalindrome(s,index,i)){
+            path.push_back(s.substr(index,i-index+1));
+            partioning(s,i+1,n,path);
+            path.pop_back();
         }
     }
-    vector<vector<string>> allPalindromicPerms(string S) {
-        vector<string> path;
-        partioning(S,0,S.length(),path);
-        return ans;
-    }
+}
+vector<vector<string>> allPalindromicPerms(string S) {
+    vector<string> path;
+    partioning(S,0,S.length(),path);
+    return ans;
+}
 };
 
 //{ Driver Code Starts.
