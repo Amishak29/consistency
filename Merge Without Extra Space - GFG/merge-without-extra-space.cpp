8cd38004745d9dc4fc6ba1ed182a53a4d1d427dd
Amisha Arunjay Kumar <78794164+Amishak29@users.noreply.github.com>
2023-09-22 17:43:01 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.cpp
+++ Merge Without Extra Space - GFG/merge-without-extra-space.cpp
@@ -6,31 +6,38 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-//Function to merge the arrays.
-void merge(long long arr1[], long long arr2[], int n, int m)
-{
-vector<int> ans;
-for(int i=0;i<n;i++)
-{
-ans.push_back(arr1[i]);
-}
-for(int j=0;j<m;j++)
-{
-ans.push_back(arr2[j]);
-}
-sort(ans.begin(),ans.end());
- int k=0;
-         for(int i=0;i<n;i++)
-        {
-            arr1[i]=ans[k++];// k ka value is changes to n now
-        }
-        for(int j=0;j<m;j++)
-        {
-            arr2[j]=ans[k++];
-        }
-
-    }
-
+        void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+            int left=0;
+            int right=0;
+            int index=0;
+            int arr3[n+m];
+            while(left<n && right<m){
+                if(arr1[left]<=arr2[right]){
+                    arr3[index]=arr1[left];
+                    left++;
+                    index++;
+                }
+                if(arr1[left]>arr2[right]){
+                    arr3[index]=arr2[right];
+                    index++,right++;
+                }
+            }
+            while(left<n){
+                arr3[index++]=arr1[left++];
+            }
+            while(right<m){
+                arr3[index++]=arr2[right++];
+            }
+            for(int i=0;i<n+m;i++){
+                if(i<n){
+                    arr1[i]=arr3[i];
+                }else{
+                    arr2[i-n]=arr3[i];
+                }
+            }
+        } 
+        
 };
 
 //{ Driver Code Starts.
