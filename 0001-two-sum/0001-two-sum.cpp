e76b63cdb0b57378846aebb24c3d15a88ef3c457
Amisha Arunjay Kumar <78794164+Amishak29@users.noreply.github.com>
2023-10-01 15:44:17 +0530

    Time: 3 ms (99.21%), Space: 11.2 MB (23.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int i,j;
-        vector<int> ans(2);
-        for(i=0;i<nums.size();i++){
-        for(j=i+1;j<nums.size();j++){
-            if(nums[i]+nums[j]==target){
-                //ans.push_back(i);
-                //ans.push_back(j);
-                ans[0]=i;
-                ans[1]=j;
-            }            
+        // Create an unordered_map to store the numbers we've seen so far.
+        unordered_map<int, int> seen;
+
+        // Loop through the input vector 'nums'.
+        for (int i = 0; i < nums.size(); ++i) {
+            int b = nums[i];         // Get the current number 'b' from the vector.
+            int a = target - b;     // Calculate the number 'a' needed to reach the target.
+
+            // Check if we've already seen 'a' in the 'seen' map.
+            if (seen.count(a)) {
+                // If we've seen 'a' before, it means we've found a pair (a, b)
+                // where a + b equals the 'target'. Return the indices of 'a' and 'b'.
+                return {seen[a], i};
+            }
+
+            // If we haven't seen 'a' yet, store the current number 'b' along with its index 'i'
+            // in the 'seen' map for future reference.
+            seen[b] = i;
         }
-        
-    }
-        return ans;
+
+        // If no pair is found, return an empty vector to indicate failure.
+        return {};
     }
 };
