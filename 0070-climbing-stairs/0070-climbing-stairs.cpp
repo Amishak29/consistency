b665d9dd71ad2693d290f4451524a65d43cc119c
Amisha Arunjay Kumar <78794164+Amishak29@users.noreply.github.com>
2024-01-18 15:12:54 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (88.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -4,7 +4,7 @@ class Solution {
     int prev1 = 1; 
     int prev2 = 1;  
 
-    for (int i = 2; i <= n; ++i) {
+    for (int i = 2; i <= n; i++) {
       int temp = prev1 + prev2;
       prev2 = prev1;
       prev1 = temp;
