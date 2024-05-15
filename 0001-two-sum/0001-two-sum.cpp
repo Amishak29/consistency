00b34e3e48fc9cc827a093bfd1e0a9e58abbe8e5
Amisha Arunjay Kumar <78794164+Amishak29@users.noreply.github.com>
2024-05-15 07:54:25 +0530

    Time: 7 ms (85.87%), Space: 13.7 MB (44.43%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // Create an unordered_map to store the numbers we've seen so far.
-        unordered_map<int, int> seen;
-
-        // Loop through the input vector 'nums'.
+        vector<pair<int, int>> res;
         for (int i = 0; i < nums.size(); ++i) {
-            int b = nums[i];         // Get the current number 'b' from the vector.
-            int a = target - b;     // Calculate the number 'a' needed to reach the target.
-
-            // Check if we've already seen 'a' in the 'seen' map.
-            if (seen.count(a)) {
-                // If we've seen 'a' before, it means we've found a pair (a, b)
-                // where a + b equals the 'target'. Return the indices of 'a' and 'b'.
-                return {seen[a], i};
-            }
-
-            // If we haven't seen 'a' yet, store the current number 'b' along with its index 'i'
-            // in the 'seen' map for future reference.
-            seen[b] = i;
+            res.push_back({nums[i], i});
         }
+        
+        sort(res.begin(), res.end());
+        
+        int left = 0;
+        int right = res.size() - 1;
 
-        // If no pair is found, return an empty vector to indicate failure.
-        return {};
+        while (left < right) {
+            int sum = res[left].first + res[right].first;
+            if (sum == target) {
+                return {res[left].second, res[right].second};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return {}; 
     }
 };
