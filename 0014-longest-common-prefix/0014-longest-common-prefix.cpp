e57e9f2c8c8b061cdf4b7a4289e51cfd85d2e6e9
Amisha Arunjay Kumar <78794164+Amishak29@users.noreply.github.com>
2024-05-16 07:29:11 +0530

    Time: 8 ms (10.31%), Space: 10.7 MB (88.56%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -3,6 +3,12 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
          string ans = "";
         // sort(strs.begin(),strs.end());
+        if(strs.empty()){
+           return ans;
+        }
+        if(strs.size()==1){
+           return strs[0];
+        }
         for(int i=0;i<strs[0].size();i++){
            char ch = strs[0][i];
             bool match =true;
